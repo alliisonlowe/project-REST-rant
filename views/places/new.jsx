887a9d7112d9a79803d1f41958b657213d4761b8
Allison Lowe <alliisonlowe@gmail.com>
2022-11-15 22:53:49 -0800

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,12 +1,40 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form () {
+function new_form() {
     return (
         <Def>
-          <main>
-            <h1>Add a New Place</h1>
-          </main>
+            <main>
+                <h1>Add a New Place</h1>
+                <form method="POST" action="/places">
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" required />
+                    </div>
+                    <p> </p>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" id="pic" name="pic" />
+                    </div>
+                    <p> </p>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" />
+                    </div>
+                    <p> </p>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" />
+                    </div>
+                    <p> </p>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input className="form-control" id="cuisines" name="cuisines" required />
+                    </div>
+                    <p> </p>
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </form>
+            </main>
         </Def>
     )
 }
